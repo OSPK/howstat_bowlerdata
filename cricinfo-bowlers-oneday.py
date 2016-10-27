2573128430958a06f06d1b87a6e467709374bd29
waqas <waqas@opensource.com.pk>
2016-10-27 14:02:49 +0500

    stats

---
+++ cricinfo-bowlers-oneday.py
@@ -0,0 +1,85 @@
+#!env/bin/python3
+# -*- coding: utf-8 -*-
+import requests, re
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///oneday-bowlers-cricinfo.db'
+db = SQLAlchemy(app)
+
+class Bowler(db.Model):
+    __tablename__ = 'bowlers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), index=True)
+    country = db.Column(db.String(80))
+    span = db.Column(db.String(80))
+    matches = db.Column(db.Integer)
+    innings = db.Column(db.Integer)
+    balls = db.Column(db.Integer)
+    runs = db.Column(db.Integer)
+    wickets = db.Column(db.Integer)
+    bbi = db.Column(db.Integer)
+    bbm = db.Column(db.Integer)
+    average = db.Column(db.Integer)
+    economy = db.Column(db.Integer)
+    strike_rate = db.Column(db.Integer)
+    fives = db.Column(db.Integer)
+    tens = db.Column(db.Integer)
+
+    def __init__(self, **kwargs):
+        super(Bowler, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return self.name + ": " + str(self.runs)
+
+db.create_all()
+
+# Fetch from Cricinfo
+def get_page(page):
+    url = "http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;filter=advanced;orderby=wickets;page={};size=200;template=results;type=bowling".format(page)
+    return url
+
+for i in range(1, 13):
+    url = get_page(i)
+    print(url)
+
+    get_results = requests.get(url)
+
+    if get_results.status_code is 200:
+
+        soup = BeautifulSoup(get_results.text, 'html.parser')
+
+        data = soup.find_all('tr', attrs={'class': 'data1'})
+
+        for row in data:
+            first_td = row.findNext('td')
+            name = first_td.text.split("(")[0].strip()
+            country = first_td.text.split("(")[1].strip(")").strip()
+            span = first_td.findNext('td')
+            matches = span.findNext('td')
+            innings = matches.findNext('td')
+            balls = innings.findNext('td')
+            runs = balls.findNext('td')
+            wickets = runs.findNext('td')
+            bbi = wickets.findNext('td')
+            bbm = bbi.findNext('td')
+            average = bbm.findNext('td')
+            economy = average.findNext('td')
+            strike_rate = economy.findNext('td')
+            fives = strike_rate.findNext('td')
+            tens = fives.findNext('td')
+
+            player = Bowler(name=name, country=country, span=span.text,
+                            matches=matches.text, innings=innings.text, balls=balls.text,
+                            runs=runs.text, wickets=wickets.text, bbi=bbi.text,
+                            bbm=bbm.text, average=average.text, economy=economy.text,
+                            strike_rate=strike_rate.text, fives=fives.text, tens=tens.text)
+
+            print(player)
+            db.session.add(player)
+            db.session.commit()
+
+# # bowlers = Bowler.query.all()
+
---
+++ howstat-bowlers-oneday.py
@@ -0,0 +1,71 @@
+#!env/bin/python3
+# -*- coding: utf-8 -*-
+import requests, re
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///oneday-bowlers-howstat.db'
+db = SQLAlchemy(app)
+
+class Bowler(db.Model):
+    __tablename__ = 'bowlers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), index=True)
+    bowled = db.Column(db.Integer)
+    caught = db.Column(db.Integer)
+    caught_behind = db.Column(db.Integer)
+    lbw = db.Column(db.Integer)
+    stumped = db.Column(db.Integer)
+    hit_wicket = db.Column(db.Integer)
+    total = db.Column(db.Integer)
+
+    def __init__(self, **kwargs):
+        super(Bowler, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return self.name
+
+db.create_all()
+# Fetch from Howstat
+
+def get_url(player):
+    url = "http://www.howstat.com.au/cricket/Statistics/Players/PlayerDismissBowlGraph_ODI.asp?PlayerID={}".format(player)
+    return url
+
+for i in range(1, 3176): #3176
+    i = '{0:04}'.format(i)
+    url = get_url(i)
+    print(url)
+
+    player_page = requests.get(url)
+
+    if player_page.status_code is 200:
+        soup = BeautifulSoup(player_page.text, 'html.parser')
+
+        player_name = soup.title.string.split(" - ")[1]
+
+        bowled = soup.find(text=re.compile('Bowled'))
+
+        if bowled is not None:
+            bowled = bowled.findNext('td').text.strip()
+            caught = soup.find(text=re.compile('Caught')).findNext('td').text.strip()
+            caught_behind = soup.find(text=re.compile('Caught Behind')).findNext('td').text.strip()
+            lbw = soup.find(text=re.compile('LBW')).findNext('td').text.strip()
+            stumped = soup.find(text=re.compile('Stumped')).findNext('td').text.strip()
+            hit_wicket = soup.find(text=re.compile('Hit Wicket')).findNext('td').text.strip()
+
+            bowler_table = {"name":player_name, "bowled":bowled, "caught":caught,
+                            "caught_behind":caught_behind, "lbw":lbw, "stumped":stumped,
+                            "hit_wicket":hit_wicket}
+
+            player = Bowler(name=player_name, bowled=bowled, caught=caught,
+                            caught_behind=caught_behind, lbw=lbw, stumped=stumped,
+                            hit_wicket=hit_wicket)
+            db.session.add(player)
+            db.session.commit()
+            print(bowler_table) #[0].strip()
+
+# bowlers = Bowler.query.all()
+
